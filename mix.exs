360e09183b809384e60415594c33c6a11b9c203e
Carlos Brito Lage <carlos@carloslage.net>
2018-04-05 19:01:31 +0100

    Woot

---
+++ mix.exs
@@ -0,0 +1,24 @@
+defmodule KillBill.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :async,
+      version: "0.1.0",
+      language: :erlang,
+      deps: deps(Mix.env()),
+      description: "Async consists on a worker pool for asynchronous execution of tasks (i.e. functions).",
+      package: package(),
+      source_url: "https://github.com/aruki-delivery/async",
+      homepage_url: "https://hex.pm/packages/async"]
+  end
+
+  defp deps(_) do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  def package do
+    [ maintainers: ["cblage"],
+      licenses: ["Apache License 2.0"],
+      links: %{"GitHub" => "https://github.com/aruki-delivery/async" } ]
+  end
+end
---
+++ package.exs
@@ -0,0 +1,10 @@
+Expm.Package.new(
+  name: "async",
+  description: "Async consists on a worker pool for asynchronous execution of tasks (i.e. functions).",
+  version: "0.1.0",
+  keywords: ["erlang", "async", "tasks"],
+  licenses: [[name: "Apache License 2.0"]],
+  maintainers: [
+    [name: "Joaquim Rocha",  email: "jrocha.github@gmailbox.org"],
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]],
+  repositories: [[github: "aruki-delivery/async", branch: "master"]])
